0b6d5f37c972cb7e5e45bebeb0359a41670d0eb0
JoaoHeron <joaoheron@gmail.com>
2020-02-12 00:57:37 -0300

    delete invalid lines at all tables

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ import service
 # Realiza um pré tratamento dos dados
 p = Parse()
 p.string_to_number(vars.sectors_tsv_path, vars.temp_tsv_path)
-p.delete_invalid_lines(vars.contacts_tsv_path, vars.temp_tsv_path)
-p.delete_invalid_lines(vars.companies_tsv_path, vars.temp_tsv_path)
+p.delete_invalid_lines(vars.temp_tsv_path)
 # Extrai scripts SQL dos arquivos .tsv
 extractor.extractSQLfromTsv()
 # Conecta com servidor MySQL, Transforma os dados e Salva os outputs em /res/outputs/
--- parse.py
+++ parse.py
@@ -19,21 +19,23 @@ class Parse:
         os.remove(infilePath)
         os.rename(outfilePath, infilePath)
 
-    def delete_invalid_lines(self, infilePath, outfilePath):
-        with open(infilePath, 'r', encoding='utf-8') as infile, open(outfilePath, 'w', encoding='utf-8') as outfile:
-            for line in infile:
-                try:
-                    # Verifica se a linha possui apenas caracteres com encoding latin1 e escreve a linha no arquivo temporário
-                    # Caso ocorra alguma exceção, o número de linhas inválidas é incrementado 
-                    line.encode('latin1')
-                    outfile.write(line)
-                except:
-                    # Se a linha não suporta encoding latin1, incrementa e printa o número de linhas invalidas
-                    self.invalid+=1
-                    print(INVALID_LINE_FOUND + str(self.invalid))
-        infile.close()
-        outfile.close()
-        # substitui o arquivo antigo pelo novo arquivo e renomeia
-        os.remove(infilePath)
-        os.rename(outfilePath, infilePath)
+    def delete_invalid_lines(self, outfilePath):
+        for infilePath in os.listdir(vars.tsv_path):
+            print('INFILE PATHN: ', infilePath)
+            with open(vars.tsv_path + infilePath, 'r', encoding='utf-8') as infile, open(outfilePath, 'w', encoding='utf-8') as outfile:
+                for line in infile:
+                    try:
+                        # Verifica se a linha possui apenas caracteres com encoding latin1 e escreve a linha no arquivo temporário
+                        # Caso ocorra alguma exceção, o número de linhas inválidas é incrementado 
+                        line.encode('latin1')
+                        outfile.write(line)
+                    except:
+                        # Se a linha não suporta encoding latin1, incrementa e printa o número de linhas invalidas
+                        self.invalid+=1
+                        print(INVALID_LINE_FOUND + str(self.invalid))
+            infile.close()
+            outfile.close()
+            # substitui o arquivo antigo pelo novo arquivo e renomeia
+            os.remove(infilePath)
+            os.rename(outfilePath, infilePath)
     
